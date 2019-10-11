641c64f6841a00b3074361c74c0b39aa48aeb3ee
EduardBaer <EduardBaer@users.noreply.github.com>
2019-10-11 20:20:37 +0200

    move snail to snail.lua

--- main.lua
+++ main.lua
@@ -1,27 +1,27 @@
-snailxpos = 0
-snailypos = 0
+require 'snail'
+
 
 function love.load()
-    snail = love.graphics.newImage("/assets/images/snail.png")
+    mysnail = Snail:new(0, 0, "/assets/images/snail.png")
     love.graphics.setNewFont(12)
     love.graphics.setBackgroundColor(255,255,255)
 end
 
 function love.update(dt)
     if love.keyboard.isDown("down") then
-        snailypos = snailypos + 100 * dt -- this would increment num by 100 per second
+        mysnail:movey(100 * dt)
     end
     if love.keyboard.isDown("up") then
-        snailypos = snailypos - 100 * dt
+        mysnail:movey(- 100 * dt)
     end
     if love.keyboard.isDown("left") then
-        snailxpos = snailxpos - 100 * dt
+        mysnail:movex(- 100 * dt)
     end
     if love.keyboard.isDown("right") then
-        snailxpos = snailxpos + 100 * dt
+        mysnail:movex(100 * dt)
     end
 end
 
 function love.draw()
-    love.graphics.draw(snail, snailxpos, snailypos)
+    mysnail:draw()
 end
---
+++ snail.lua
@@ -0,0 +1,23 @@
+Snail = {}
+Snail.__index = Snail
+
+function Snail:new(x, y, image)
+    local sn = {}
+    setmetatable(sn, Snail)
+    sn.x = x
+    sn.y = y
+    sn.image = love.graphics.newImage(image)
+    return sn
+end
+
+function Snail:movex(p)
+    self.x = self.x + p
+end
+
+function Snail:movey(p)
+    self.y = self.y + p
+end
+
+function Snail:draw()
+    love.graphics.draw(self.image, self.x, self.y)
+end
