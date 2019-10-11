6b17a4a7fc417cbc9fd12be03afa1c2069611498
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-10-11 19:33:29 +0200

    Add first snail image. It's a magic snail which looks like a cat at the moment!

--- main.lua
+++ main.lua
@@ -1,3 +1,27 @@
+snailxpos = 0
+snailypos = 0
+
+function love.load()
+    snail = love.graphics.newImage("/assets/images/snail.png")
+    love.graphics.setNewFont(12)
+    love.graphics.setBackgroundColor(255,255,255)
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("down") then
+        snailypos = snailypos + 100 * dt -- this would increment num by 100 per second
+    end
+    if love.keyboard.isDown("up") then
+        snailypos = snailypos - 100 * dt
+    end
+    if love.keyboard.isDown("left") then
+        snailxpos = snailxpos - 100 * dt
+    end
+    if love.keyboard.isDown("right") then
+        snailxpos = snailxpos + 100 * dt
+    end
+end
+
 function love.draw()
-    love.graphics.print("Hello World!", 400, 300)
+    love.graphics.draw(snail, snailxpos, snailypos)
 end
