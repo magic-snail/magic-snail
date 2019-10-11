d99a887463d6bbdce8e1a53bca4f2c647160f3d9
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-10-11 22:55:56 +0200

    snail moves in every direction

--- main.lua
+++ main.lua
@@ -5,9 +5,8 @@ function love.load()
     points = 0
     startTime = love.timer.getTime()
     speed = 1000
-
-    -- Backgroundimage
-    myBackground = love.graphics.newImage("/assets/images/green.png")
+    numGrass = 10
+    numMushroom = 10
 
     -- Backgroundsound
     myBackgroundSound = love.audio.newSource("/assets/music/sneaky_snitch.mp3", "stream")
@@ -20,8 +19,28 @@ function love.load()
     snailIcon = love.image.newImageData('/assets/images/snail_left.png')
     love.window.setIcon(snailIcon)
 
+    -- Backgroundimage
+    myBackground = love.graphics.newImage("/assets/images/green.png")
+    -- Array with grass
+    myBackgroundGrass = love.graphics.newImage("/assets/images/green_grass.png")
+    backgroundGrassArray = {}
+    for i = 1, numGrass + 1 do
+        backgroundGrassArray[i] = {}
+        backgroundGrassArray[i]["x"] = love.math.random(love.graphics.getWidth())
+        backgroundGrassArray[i]["y"] = love.math.random(love.graphics.getHeight())
+    end
+
+    -- Array with mushrooms
+    myBackgroundMush= love.graphics.newImage("/assets/images/green_mushroom.png")
+    backgroundMushArray = {}
+    for i = 1, numMushroom + 1 do
+        backgroundMushArray[i] = {}
+        backgroundMushArray[i]["x"] = love.math.random(love.graphics.getWidth())
+        backgroundMushArray[i]["y"] = love.math.random(love.graphics.getHeight())
+    end
+
     -- Classes
-    mySnail = Snail:new(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, "/assets/images/snail_left.png")
+    mySnail = Snail:new(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, "/assets/images/snail_left.png", "/assets/images/snail_right.png", "/assets/images/snail_back.png", "/assets/images/snail_front.png")
 end
 
 function love.update(dt)
@@ -46,6 +65,16 @@ function love.draw()
         for j = 0, love.graphics.getHeight() / myBackground:getHeight() do
             love.graphics.draw(myBackground, i * myBackground:getWidth(), j * myBackground:getHeight())
         end
+    end
+
+    -- Draw Grass
+    for i, position in pairs(backgroundGrassArray) do
+        love.graphics.draw(myBackgroundGrass, position["x"], position["y"])
+    end
+
+    -- Draw Mushrooms
+    for i, position in pairs(backgroundMushArray) do
+        love.graphics.draw(myBackgroundMush, position["x"], position["y"])
     end
 
     -- print FPS
--- snail.lua
+++ snail.lua
@@ -1,13 +1,16 @@
 Snail = {}
 Snail.__index = Snail
 
-function Snail:new(x, y, image)
+function Snail:new(x, y, imageLeft, imageRight, imageUp, imageDown)
     local sn = {}
     setmetatable(sn, Snail)
     sn.x = x
     sn.y = y
-    sn.image = love.graphics.newImage(image)
-    sn.snailWidth, sn.snailHeight = sn.image:getDimensions()
+    sn.imageLeft = love.graphics.newImage(imageLeft)
+    sn.imageRight = love.graphics.newImage(imageRight)
+    sn.imageUp = love.graphics.newImage(imageUp)
+    sn.imageDown = love.graphics.newImage(imageDown)
+    sn.snailWidth, sn.snailHeight = sn.imageLeft:getDimensions()
 
     -- 0 = right, 1 = down, 2 = left, 3 = up
     sn.facingDirection = 0
@@ -15,6 +18,7 @@ function Snail:new(x, y, image)
 end
 
 function Snail:moveX(p)
+    -- 0 = right, 1 = down, 2 = left, 3 = up
     if p < 0 then self.facingDirection = 2 else self.facingDirection = 0 end
 
     self.x = self.x + p
@@ -29,6 +33,9 @@ function Snail:moveX(p)
 end
 
 function Snail:moveY(p)
+    -- 0 = right, 1 = down, 2 = left, 3 = up
+    if p < 0 then self.facingDirection = 3 else self.facingDirection = 1 end
+
     self.y = self.y + p
 
     -- Stop at borders
@@ -42,8 +49,8 @@ end
 
 function Snail:draw()
     -- 0 = right, 1 = down, 2 = left, 3 = up
-    if self.facingDirection == 0 then love.graphics.draw(self.image, self.x, self.y, 0, -1, 1) end
-    -- if self.facingDirection == 1 then love.graphics.draw(self.image, self.x, self.y, 0, -1, 1) end
-    if self.facingDirection == 2 then love.graphics.draw(self.image, self.x, self.y, 0, 1, 1) end
-    -- if self.facingDirection == 3 then love.graphics.draw(self.image, self.x, self.y, 0, -1, 1) end
+    if self.facingDirection == 0 then love.graphics.draw(self.imageRight, self.x, self.y) end
+    if self.facingDirection == 1 then love.graphics.draw(self.imageDown, self.x, self.y) end
+    if self.facingDirection == 2 then love.graphics.draw(self.imageLeft, self.x, self.y) end
+    if self.facingDirection == 3 then love.graphics.draw(self.imageUp, self.x, self.y) end
 end
