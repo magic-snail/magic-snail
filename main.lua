ceaa166ffb25ff2a6d28e8d53e3b5f80a9f97da0
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-10-11 21:19:41 +0200

    Add Background, add FPS

--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
 require 'snail'
 
-
 function love.load()
     speed = 1000
     mySnail = Snail:new(0, 0, "/assets/images/snail.png")
+    myBackground = love.graphics.newImage("/assets/images/green.png")
+
     love.window.setFullscreen(true)
     love.graphics.setNewFont(12)
     love.graphics.setBackgroundColor(255,255,255)
@@ -26,5 +27,14 @@ function love.update(dt)
 end
 
 function love.draw()
+    for i = 0, love.graphics.getWidth() / myBackground:getWidth() do
+        for j = 0, love.graphics.getHeight() / myBackground:getHeight() do
+            love.graphics.draw(myBackground, i * myBackground:getWidth(), j * myBackground:getHeight())
+        end
+    end
+
+    -- print FPS
+    love.graphics.print("FPS: " .. love.timer.getFPS(), 0, 0)
+
     mySnail:draw()
 end
