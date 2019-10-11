3b12a79feb17d0d3b0558a735b27459667a6e5f6
david <github@esht.de>
2019-10-11 23:05:23 +0200

    elements

---
+++ element.lua
@@ -0,0 +1,26 @@
+Element = {}
+Element .__index = Element
+
+function Element:new(x, y, directionX, directionY,image)
+    local em = {}
+    setmetatable(em, Element)
+    em.x = x
+    em.y = y
+    em.dx = directionX
+    em.dy = directionY
+    em.speed = speed
+    em.image = love.graphics.newImage(image)
+    return em
+end
+
+function Element:fire(p)
+    dir = math.atan2(self.dx-self.x,self.dy-self.y)
+    ax = p * math.cos(dir)
+    by = p * math.sin(dir)
+    self.x = self.x + by
+    self.y = self.y + ax
+end
+
+function Element:draw()
+    love.graphics.draw(self.image, self.x, self.y)
+end
