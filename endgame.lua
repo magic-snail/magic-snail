f574ff69b506ad3fd12f7567fe8f5592c6a4e36e
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-10-12 19:59:51 +0200

    Highscores!

--- endgame.lua
+++ endgame.lua
@@ -1,3 +1,5 @@
+local http = require("socket.http")
+
 endgame = {}
 
 local endpoints
@@ -5,13 +7,25 @@ function endgame.load(points)
     love.graphics.setNewFont(25)
     love.graphics.setBackgroundColor(0, 0, 0)
     endpoints = points
+
+    highscores, b, c = http.request("http://tclinux.de/snailscore.php?password=Geix7ei3oZey3reiJi6c&mode=get")
+    http.request("http://tclinux.de/snailscore.php?password=Geix7ei3oZey3reiJi6c&mode=set&score=" .. points)
 end
 
 function endgame.update()
 end
 
 function endgame.draw()
-    love.graphics.print('YouLost, points: ' .. endpoints)
+    love.graphics.print('You have been eaten! Your points: ' .. endpoints, 0, 0)
+
+    love.graphics.print('WORLDWIDE HIGHSCORES: ', 0, 50)
+    i = 1
+    for k, v in string.gmatch(highscores, "(%w+);(%w+)") do
+        love.graphics.print(k, 0, 0 + (100*i))
+        love.graphics.print(v, 0, 50 + (100*i))
+        i = i + 1
+    end
 end
 
 return endgame
+
