76abdff89c2ae6c73eb0643b596e56730024dbbe
Bernd Stellwag <burned@zerties.org>
2019-10-12 19:42:38 +0200

    set version number in love config and disable physics enging since we're not using it

--- conf.lua
+++ conf.lua
@@ -2,4 +2,6 @@ function love.conf(t)
     t.window.title = 'Magic Snail'
     t.window.fullscreen = true
     t.window.icon = '/assets/images/snail_left.png'
+    t.modules.physics = false
+    t.version = "11.1"
 end
