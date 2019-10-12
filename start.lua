9c690172be0c1095daf7da375b245b452172fb82
EduardBaer <EduardBaer@users.noreply.github.com>
2019-10-12 15:41:55 +0200

    add logo to start screen

--- start.lua
+++ start.lua
@@ -1,6 +1,10 @@
 start = {}
 
+local logoImage
+
 function start.load()
+    love.graphics.setBackgroundColor(255, 255, 255)
+    logoImage = love.graphics.newImage('/assets/images/snail_icon.png')
 end
 
 function start.update()
@@ -12,7 +16,12 @@ function start.update()
 end
 
 function start.draw()
-    love.graphics.print('Hello World!')
+    local windowWidth, windowHeight, _
+    local logoX, logoY
+    windowWidth, windowHeight, _ = love.window.getMode()
+    logoX = windowWidth / 2 - logoImage:getWidth() / 2
+    logoY = windowHeight / 2 - logoImage:getHeight() / 2
+    love.graphics.draw(logoImage, logoX, logoY)
 end
 
 return start
