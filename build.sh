1e787cbb8844123bbd960121a1fe7a244525ce48
Bernd Stellwag <burned@zerties.org>
2019-10-12 20:04:23 +0200

    improve build script output

--- build.sh
+++ build.sh
@@ -5,17 +5,21 @@ set -e
 rm -rf dist/
 mkdir dist/
 
-zip -9 -r dist/magic-snail.love . \
+echo -n 'Building magic-snail.love ...'
+zip -q -9 -r dist/magic-snail.love . \
     --exclude=*.git/* \
     --exclude=*.idea/* \
     --exclude=*dist/*
+echo 'done.'
 
 cd dist/
 
 # build for windows
+echo -n 'Building for windows ...'
 wget -q https://bitbucket.org/rude/love/downloads/love-11.2-win64.zip -O love.zip
 unzip -q love.zip
 rm love.zip
 cat love-11.2.0-win64/love.exe magic-snail.love > love-11.2.0-win64/magic-snail.exe
-zip -q -9 -r magic-snail-win64.zip love-11.2.0-win64
+zip -q -j -9 -r magic-snail-win64.zip love-11.2.0-win64
 rm -r love-11.2.0-win64
+echo 'done.'
