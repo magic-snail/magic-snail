b0375f1a3352a48b5326000faa2e98554402a163
EduardBaer <EduardBaer@users.noreply.github.com>
2019-10-12 16:49:32 +0200

    add hint to press space to start screen

--- start.lua
+++ start.lua
@@ -1,8 +1,14 @@
 start = {}
 
+local startFont
+local textColor
+local coloredText
 local logoImage
 
 function start.load()
+    startFont = love.graphics.setNewFont(25)
+    textColor = {0, 0, 0}
+    coloredText = {textColor, "Press SPACE to start!"}
     love.graphics.setBackgroundColor(255, 255, 255)
     logoImage = love.graphics.newImage('/assets/images/snail_icon.png')
 end
@@ -22,6 +28,10 @@ function start.draw()
     logoX = windowWidth / 2 - logoImage:getWidth() / 2
     logoY = windowHeight / 2 - logoImage:getHeight() / 2
     love.graphics.draw(logoImage, logoX, logoY)
+
+    love.graphics.setFont(startFont)
+    local textYPos = windowHeight / 2 + logoImage:getHeight() / 2 + 100
+    love.graphics.printf(coloredText, 0, textYPos, windowWidth, "center")
 end
 
 return start
