432f508dba421a837f88d5898f4b39c0b5b4d377
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-10-13 11:06:14 +0200

    timeout http operation

--- endgame.lua
+++ endgame.lua
@@ -1,4 +1,5 @@
 local http = require("socket.http")
+http.TIMEOUT = 5
 
 endgame = {}
 
@@ -30,16 +31,18 @@ function endgame.draw()
 
     love.graphics.print('You have been eaten! Your points: ' .. endpoints, logoX, logoY + 20)
 
-    love.graphics.print('WORLDWIDE HIGHSCORES: ', logoX, logoY + 70)
     local i = 1
-    for k, v in string.gmatch(highscores, "(%w+);(%w+)") do
-        love.graphics.print(i .. ": ", logoX, logoY + 70 + (35*i))
-        love.graphics.print(k, logoX + 50, logoY + 70 + (35*i))
-        love.graphics.print(i+1 .. ": ", logoX, logoY + 105 + (35*i))
-        love.graphics.print(v, logoX + 50, logoY + 105 + (35*i))
-        i = i + 2
+    if highscores then
+        love.graphics.print('WORLDWIDE HIGHSCORES: ', logoX, logoY + 70)
+        for k, v in string.gmatch(highscores, "(%w+);(%w+)") do
+            love.graphics.print(i .. ": ", logoX, logoY + 70 + (35*i))
+            love.graphics.print(k, logoX + 50, logoY + 70 + (35*i))
+            love.graphics.print(i+1 .. ": ", logoX, logoY + 105 + (35*i))
+            love.graphics.print(v, logoX + 50, logoY + 105 + (35*i))
+            i = i + 2
+        end
     end
-    if points ~= 0 then
+    if place then
         love.graphics.print('You are on place: ' .. place, logoX, logoY + 90 + (35*i))
     end
 end
